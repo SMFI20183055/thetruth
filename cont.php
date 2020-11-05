105974eb0d3d1feeaf5e14e9364ee2a15d2127af
SMFI20183055 <61048092+SMFI20183055@users.noreply.github.com>
2020-11-05 16:56:01 +0530

    Add files via upload

--- cont.php
+++ cont.php
@@ -10,6 +10,7 @@ include("ff1.php");
 <head>
 	<title>The Truth</title>
 	<meta charset=utf-8>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet">	
 	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
 	<link rel="stylesheet" type="text/css" href="style1.css">
@@ -19,7 +20,68 @@ include("ff1.php");
 	<link rel="shortcut icon" type="image/png" href="bd.png">
 </head>
 <style>
+@media only screen and (min-width: 600px){
+body {
+	width: 100%;
+	height: 100%;
+  }
+ }
 
+@media only screen and (max-width: 300px){
+body {
+    width: 100%;
+    height: 100%;
+    margin: 0px 9px 9px -450px;
+    background-size: 103% 105%;
+  }
+  header{
+    height: 1%;
+    width: 129%;
+    margin: 0px 13px 24px -27px;
+  }
+  nav {
+     margin: 7px 11px 20px 520px;
+    height: 31%;
+    width: 53%;
+    font-size: 10px;
+  }
+  nav .menu a {
+  	color:  red;
+	margin: -1px 20px -43px 3px;
+  }
+  .foot {
+  	height: 46%;
+    width: 50%;
+    margin: 51px 2px -6px 498px;
+  }
+  .log {
+  	height: 30%;
+	width: 100%;
+	margin: 11px 10px 8px 8px; 
+  }
+  .menu {
+  	    margin: 32px 30px 30px 30px;
+  }
+  .ff {
+	margin: 51px -8px -18px 508px;
+    height: 474px;
+    width: 65%;
+  }
+  input {
+  	height: 38px;
+    width: 261px !important;
+  }
+  .f1 {
+  	height: 35px;
+    width: 185px !important;
+  }
+  .comm {
+  	    width: 183px;
+  }
+  .btn {
+  	    width: 188px !important;
+  }
+}
 </style>
 <body>
 
@@ -28,10 +90,12 @@ include("ff1.php");
     <div class="log">
 	  <h1 class="animated infinite heartBeat ">The Truth</h1> </div>
 		 <div class="menu">
-		 <a href="index.html" target="_blank">Home</a>
-		 <a href="#">Gallery</a>
-		 <a href="#">Contact</a>
-		 <a href="#">About</a> <!-- div is  closed -->
+		 <ul>
+		<li><a href="index.html" target="_blank">Home</a></li> 
+		 <li> <a href="#">Gallery</a></li> 
+		<li> </li>  <a href="#">Contact</a></li> 
+		<li>  <a href="#">About</a></li>  <!-- div is  closed -->
+	</ul>
 	</div>
 </nav>
 </header>
